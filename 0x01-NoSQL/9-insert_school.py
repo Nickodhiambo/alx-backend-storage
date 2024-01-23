86b0cef6eb31b7af9ca7e2fa9483632ba82eafd3
Nick <nodhiambo01@gmail.com>
2024-01-22 23:16:35 -0800

    Insert with pymongo

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -7,4 +7,5 @@ import pymongo
 
 def insert_school(mongo_collection, **kwargs):
     """Inserts a document into a collection"""
-    return mongo_collection.insert(kwargs)
+    if mongo_collection is not None:
+        return mongo_collection.insert_one(kwargs).inserted_id
